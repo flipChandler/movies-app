704c00505252949afb1a12ce5155795b804e8d23
flipChandler <felipe.santos11@hotmail.com>
2021-11-19 19:32:17 -0300

    feat service para validar campos no form

---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    if ((control.dirty || control.touched) && this.hasError(control, errorName)) {
+      return true;
+    }
+    return false;
+  }
+
+
+
+  // método para simplificar as repetições de erro no HTML
+  hasError(control: AbstractControl, errorName: string): boolean {
+    return control.hasError(errorName);
+  }
+
+  
+}
