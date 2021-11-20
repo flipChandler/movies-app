bad5e6d3cc3ea4f2827922381ecae65f65f098ba
flipChandler <felipe.santos11@hotmail.com>
2021-11-20 00:01:51 -0300

    feat array de generos de filmes

--- src/app/shared/components/campos/input-select/input-select.component.ts
+++ src/app/shared/components/campos/input-select/input-select.component.ts
@@ -12,6 +12,7 @@ export class InputSelectComponent {
   @Input() titulo: string;
   @Input() formGroup: FormGroup;
   @Input() controlName: string;
+  @Input() opcoes: Array<string>; // vira uma propriedade one-way-data-bind do component, [opcoes]
 
   constructor(public validaCamposSvc: ValidarCamposService) { }
 
